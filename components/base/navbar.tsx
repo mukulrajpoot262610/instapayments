860ba800ef46dc33c6fc384d749772531a77700b
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2024-03-12 18:05:35 +0530

    add: navbar

---
+++ components/base/navbar.tsx
@@ -0,0 +1,31 @@
+import { ShoppingCartIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { Button } from '../ui/button';
+
+const Navbar = () => {
+  return (
+    <div className='mr-4 border-b fixed w-full z-50 h-20 flex items-center'>
+      <div className='max-w-6xl mx-auto flex items-center justify-between w-full'>
+        <Link href='/' className='mr-6 flex items-center space-x-2'>
+          <Image
+            src='https://groww.in/groww-logo-270.png'
+            height={100}
+            width={100}
+            alt='Logo'
+            className='h-8 w-8'
+          />
+        </Link>
+        <Button variant='ghost' className='relative'>
+          <span className='absolute h-5 w-5 bg-blue-500 rounded-full flex items-center justify-center text-xs font-bold -top-2 -right-2 text-white'>
+            2
+          </span>
+          <ShoppingCartIcon className='h-6 w-6' />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
